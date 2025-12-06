40f360c54038a141e2f54bf869632e1ef277afe2
Aske0772 <eaa25aths@students.eaaa.dk>
2025-12-06 16:26:52 +0100

    JS korrektion - buttons skifter vha. funktion

--- README.md
+++ README.md
@@ -6,3 +6,15 @@
 2. Nav var parent og gjorde at video ikke kunne blive fuld størrelse.
 3. køb nu billede lavet om til button
 
+## Navigationsbar
+1. checkbox med input hamburger lavet i css med span i stedet for billede.
+2. 
+
+## card-holder højtaler skifter billede
+1. class colour btn lavet først til at style alle buttons.
+2. class buttons lavet hver for sig til at style individuelt.
+
+
+# JavaScript
+1. brugt funktion der tager fat i src og textcontent til at ændre figure og figcaption inde i rektangle placeholder.
+2. 
--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 "use strict";
+
+// højtaler skifter billede og navn ved klik på knapper
+const productImage = document.getElementById('productImage');
+const productName = document.getElementById('productName');
+
+// genbrugelig funktion til at ændre højtaler farve
+function btnColourChange(image, name) {
+    // ændre src og text content
+    productImage.src = image;
+    productName.textContent = name;
+}
+
+
+// når man trykker på knapperne tager bruger den funktion som siger at den skal ændre src og text content
+document.querySelector('.misty-white-btn').addEventListener('click', function () {
+    btnColourChange('img/speakers/misty-white-transparent.png', 'Misty White');
+});
+
+document.querySelector('.lavender-mist-btn').addEventListener('click', function () {
+    btnColourChange('img/speakers/lavender-mist-transparent.png', 'Lavender Mist');
+});
+
+document.querySelector('.sage-green-btn').addEventListener('click', function () {
+    btnColourChange('img/speakers/sage-green-transparent.png', 'Sage Green');
+});
+
+document.querySelector('.dusty-rose-btn').addEventListener('click', function () {
+    btnColourChange('img/speakers/dusty-rose-transparent-true.png', 'Dusty Rose');
+});
