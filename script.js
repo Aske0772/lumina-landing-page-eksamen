b0639e7ad98a4b83477b5396b32021203950a70c
Aske0772 <eaa25aths@students.eaaa.dk>
2025-12-07 22:04:16 +0100

    trustpilot article section added - needs css

--- script.js
+++ script.js
@@ -68,14 +68,15 @@ const trustPilotContainer = document.querySelector('.bg-rectangle-section-trustp
 
 // backticks for at kunne lave flere strings på en gang
 // funktion til at lave trust pilot rectangles
-function trustPilotRectangle(imageSrc, starsSrc, review, author, speaker) {
+function trustPilotRectangle(imageSrc, starsSrc, review, author, speaker, purchased) {
     return `
     <article class="trust-pilot-rectangle">
         <img src="${imageSrc}" alt="Trustpilot Rating">
         <img src="${starsSrc}" alt="5 Stjerner truspilot rating">
-        <h4>${review}</h4>
-        <h5>${author}</h5>
-        <h6>${speaker}</h6>
+        <p>${review}</p>
+        <p>${author}</p>
+        <p>${speaker}</p>
+        <p>${purchased}</p>
     </article>
     `;
 }
@@ -83,55 +84,68 @@ function trustPilotRectangle(imageSrc, starsSrc, review, author, speaker) {
 // indsætter rectangles i trust pilot container
 const trustPilotData = [
     {
-    imageSrc: 'img/selfie-women/selfie-woman-one.png',
+    imageSrc: 'img/selfie-women/woman-one.png',
     starsSrc: 'img/trustpilot/5-stjerner.png',
-    review: 'Helt ærligt den bedste højtaler jeg nogensinde har ejet! Lyden er krystalklar og bassen er dyb og fyldig. Designet er også utroligt stilfuldt og passer perfekt ind i mit hjem.',
+    review: 'Helt ærligt den bedste højtaler jeg nogensinde har ejet! Lyden er krystalklar og bassen er dyb og fyldig. Designet er også utroligt stilfuldt og passer perfekt ind i mit hjem',
     author: 'Anna K.',
-    speaker: ''
+    speaker: 'misty white',
+    purchased: 'december 2025'
 
     },
 
  {
-    imageSrc: 'img/selfie-women/selfie-woman-one.png',
+    imageSrc: 'img/selfie-women/woman-two.png',
     starsSrc: 'img/trustpilot/5-stjerner.png',
-    review: '',
-    author:'',
-    speaker: ''
-    },
-
- {
-    imageSrc: 'img/selfie-women/selfie-woman-one.png',
-    starsSrc: 'img/trustpilot/5-stjerner.png',
-    review: '',
-    author:'',
-    speaker: ''
+    review: 'Jeg er virkelig imponeret over denne højtaler. Den har en fantastisk lydkvalitet og er super nem at bruge. Jeg kan forbinde den til min telefon på ingen tid, og batteritiden er imponerende lang',
+    author:'mette j.',
+    speaker: 'dusty rose',
+    purchased: 'januar 2026'
 
     },
 
  {
-    imageSrc: 'img/selfie-women/selfie-woman-one.png',
+    imageSrc: 'img/selfie-women/woman-three.png',
     starsSrc: 'img/trustpilot/5-stjerner.png',
-    review: '',
-    author:'',
-    speaker: ''
+    review: 'Den er bare let at tage med overalt, har ikke mere at sige! 5 ud af 5 stjerner',
+    author:'maria L',
+    speaker: 'lavender mist',
+    purchased: 'februar 2025'
 
     },
 
  {
-    imageSrc: 'img/selfie-women/selfie-woman-one.png',
+    imageSrc: 'img/selfie-women/woman-four.png',
     starsSrc: 'img/trustpilot/5-stjerner.png',
-    review: '',
-    author:'',
-    speaker: ''
+    review: 'den er helt fantastisk!',
+    author:'Anna S',
+    speaker: 'Sage Green',
+    purchased: 'marts 2025'
 
     },
 
  {
-    imageSrc: 'img/selfie-women/selfie-woman-one.png',
+    imageSrc: 'img/selfie-women/woman-five.png',
     starsSrc: 'img/trustpilot/5-stjerner.png',
-    review: '',
-    author:'',
-    speaker: ''
+    review: 'Virkelig god til pengene. kan varmt anbefale til alle der elsker et godt design',
+    author:'Astrid P',
+    speaker: 'misty white',
+    purchased: 'februar 2025'
 
     },
-]
+
+ {
+    imageSrc: 'img/selfie-women/woman-six.png',
+    starsSrc: 'img/trustpilot/5-stjerner.png',
+    review: 'Godt design, god lyd, og virkelig let at tage med rundt',
+    author:'Anne M',
+    speaker: 'sage green',
+    purchased: 'marts 2025'
+
+    },
+];
+
+trustPilotData.forEach(function(data) {
+    trustPilotContainer.innerHTML += trustPilotRectangle(data.imageSrc, data.starsSrc, data.review, data.author, data.speaker, data.purchased);
+});
+
+console.log(trustPilotContainer);
